329636f72302a51876784ed8f4414208b3b073bf
OsWiDev <oswielcrack@gmail.com>
2025-06-04 17:41:43 -0400

    Carta de película destacada creada y home actualizado

---
+++ app/cine/components/ratedCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import MovieCard from "./movieCard";
+const movie = {
+  id: 25,
+  title: "Destino final",
+  description: "Description for Movie 3",
+  imageUrl:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLQDApSHnbeOpFQvWxaY47k0rI3DLr1F4Fg&s",
+};
+
+const RatedCard = () => {
+  return (
+    <div>
+      <div className="">
+        <div className="flex">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLQDApSHnbeOpFQvWxaY47k0rI3DLr1F4Fg&s"
+            alt="Movie Poster"
+            className="w-full h-120 object-cover blur-xs"
+          />
+          <div className="absolute px-16 py-22 w-full flex">
+            <MovieCard
+            key={movie.id}
+            title={movie.title}
+            description={movie.description}
+            imageUrl={movie.imageUrl}
+            href={`/movies/${movie.id}`}></MovieCard>
+            <div className="py-8 px-8">
+              <h2 className="text-white text-2xl">Destacada de la semana</h2>
+              <h2 className="text-red-500 text-3xl mt-1">Destino final</h2>
+              <button className="px-4 py-2 bg-red-500 text-white hover:bg-red-700 mt-4">
+                Comprar Boletos
+              </button>
+              <button className="px-4 py-2 bg-gray-400 text-white hover:bg-gray-600 ml-4">
+                Ver Trailer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RatedCard;
--- app/cine/page.tsx
+++ app/cine/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
+import RatedCard from "./components/ratedCard";
+import CarruselAdds from "./components/addsCarousel";
+import CarruselPeliculas from "./components/movieCarousel";
 
 export default function Home() {
   return (
     <div>
-      
+      <Navbar />
+      <div>
+      <RatedCard />
+      <div className="py-6">
+        <CarruselAdds />
+      </div>
+      <div className="py-6">
+        <h1 className="text-2xl font-bold mb-4 px-4">Estrenos</h1>
+        <CarruselPeliculas />
+      </div>
+      <div className="py-6">
+        <CarruselAdds />
+      </div>
+      <div  className="py-6">
+        <h1 className="text-2xl font-bold mb-4 px-4">Todas las peliculas</h1>
+        <CarruselPeliculas />
+      </div>
+    </div>
     </div>
   );
 }
