7b2e25441d2b90e7189f89d6a3349e82a04e7f16
israelapaza <apaza.6924406@gmail.com>
2025-06-05 03:48:20 -0400

    Componente candyCarusel

---
+++ app/cine/components/candyCarusel.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import CandyCard from "./candyCard";
+
+const candys = [
+  {
+    id: 3,
+    title: 'Salchipapa',
+    description: 'Description for candy1',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 7,
+    title: 'Big hooper',
+    description: 'Description for candy2',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 19,
+    title: 'Hamburguesa con queso',
+    description: 'Description for candy3',
+    imageUrl: 'https://via.placeholder.com/150'
+  }
+];
+
+const CarruselCandy = () => {
+  return (
+    <div className="py-4">
+      <h1 className="text-2xl font-bold mb-4 px-4">Productos Recomendados</h1>
+      <div className="flex overflow-x-auto space-x-4">
+        {candys.map((candys) => (
+          <CandyCard
+            key={candys.id}
+            title={candys.title}
+            description={candys.description}
+            imageUrl={candys.imageUrl}
+            href={`/candys/${candys.id}`} // CORREGIDO
+          />
+        ))}
+        <div className="absolute justify-between mt-32 flex w-full"> {/* mt-34 corregido a mt-32 */}
+          <button className="bg-gray-500 text-white rounded-lg hover:bg-blue-600">
+            Previous
+          </button>
+          <button className="bg-gray-500 text-white rounded-lg hover:bg-blue-600 ml-2">
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarruselCandy;
