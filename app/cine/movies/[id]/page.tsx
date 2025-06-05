29bfbfd56838590dc97cb92c1f416098dfb626ec
OsWiDev <oswielcrack@gmail.com>
2025-06-05 00:38:23 -0400

    Componente acordion incorporado

--- app/cine/movies/[id]/page.tsx
+++ app/cine/movies/[id]/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import SeleccionAsientosPage from "../../components/scenary";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const moviePage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -13,6 +19,14 @@ const moviePage = ({ params }: { params: { id: string } }) => {
           className="rounded-lg"
         />
       </div>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
       <div className="justify-center items-center flex flex-col mt-4">
         <h1>Escenario</h1>
         <SeleccionAsientosPage />
