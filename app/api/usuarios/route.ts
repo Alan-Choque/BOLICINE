3a2e9cd60374c14fa152a9f9369bdc5fe52fd1b1
Alan Choque <victorchoque092@gmail.com>
2025-06-06 12:40:46 -0400

    feat: agrega la funcion para los profiles

--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -1,42 +1,48 @@
-import { currentUser } from "@/lib/auth";
+// pages/api/usuario.ts
 import { db } from "@/lib/database";
 import { NextResponse } from "next/server";
+import { auth } from "@/auth";
 
 export async function POST(req: Request) {
-  const user = await currentUser();
-  const { nombre, foto_perfil } = await req.json();
-
-  if (!user || !user.id) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
-
-  if (!nombre || !foto_perfil) {
-    return new NextResponse("Invalid data", { status: 400 });
-  }
-
   try {
-    await db.execute(
-      "UPDATE usuarios SET nombre = ?, foto_perfil = ? WHERE id_usuario = ?",
-      [nombre, foto_perfil, user.id]
+    const session = await auth();
+    if (!session?.user?.email) {
+      return NextResponse.json({ error: "No autorizado" }, { status: 401 });
+    }
+
+    const body = await req.json();
+    const { nombre, foto_perfil } = body;
+
+    // Insertar nuevo perfil en la tabla usuario
+    await db.query(
+      `INSERT INTO usuario (nombre, email, password, rol, fecha_registro, foto_perfil, verificado)
+        VALUES (?, ?, ?, ?, NOW(), ?, ?)`,
+      [
+        nombre,
+        session.user.email, // Asocia el perfil al email
+        "", // contraseña vacía por ahora
+        "perfil", // o lo que uses para roles
+        foto_perfil,
+        false,
+      ]
     );
-    return NextResponse.json({ success: true });
+
+    return NextResponse.json({ message: "Perfil creado" });
   } catch (error) {
     console.error(error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return NextResponse.json({ error: "Error en el servidor" }, { status: 500 });
   }
 }
-
-export async function DELETE() {
-  const user = await currentUser();
-  if (!user || !user.id) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
-
+export async function DELETE(req: Request) {
   try {
-    await db.execute("DELETE FROM usuarios WHERE id_usuario = ?", [user.id]);
-    return NextResponse.json({ success: true });
+    const body = await req.json();
+    const { id_usuario } = body;
+
+    await db.query(`DELETE FROM usuario WHERE id_usuario = ?`, [id_usuario]);
+
+    return NextResponse.json({ message: "Perfil eliminado" });
   } catch (error) {
     console.error(error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return NextResponse.json({ error: "Error al eliminar" }, { status: 500 });
   }
 }
