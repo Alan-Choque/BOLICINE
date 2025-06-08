ea5dfa62fae98b7a7089c1e6c948e5bcf3f46d40
OsWiDev <oswielcrack@gmail.com>
2025-06-07 22:10:42 -0400

    Metodo de pago incorporado en la ruta pagos

--- app/cine/(routes)/(id routes)/movies/[id]/page.tsx
+++ app/cine/(routes)/(id routes)/movies/[id]/page.tsx
@@ -8,19 +8,12 @@ import {
 } from "@/components/ui/accordion";
 import Details from "@/app/cine/components/details";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const moviePage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
   return (
-    <div className="text-black">
-      moviePage: {id}
-      <div className="mt-4">
-        <img
-          src={`https://via.placeholder.com/150?text=Movie+${id}`}
-          alt={`Movie ${id}`}
-          className="rounded-lg"
-        />
-      </div>
+    <div className="text-black py-15">
       <div className="px-10 py-4">
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
@@ -36,7 +29,7 @@ const moviePage = ({ params }: { params: { id: string } }) => {
         <SeleccionAsientosPage />
       </div>
       <div className="flex justify-center items-center m-4">
-        <Button>Reservar</Button>
+        <Button asChild className="bg-red-500"><Link href="/cine/pagos">Continuar</Link></Button>
       </div>
     </div>
   );
